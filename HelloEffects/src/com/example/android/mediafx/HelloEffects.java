d9319df46d07550dfc9cbaa6b22d2144941e0501
calin.bodnar <calin.bodnar@fortech.ro>
2012-10-08 17:47:42 +0300

    Fixed compiler error.

--- HelloEffects/src/com/example/android/mediafx/HelloEffects.java
+++ HelloEffects/src/com/example/android/mediafx/HelloEffects.java
@@ -32,7 +32,6 @@ import android.media.effect.EffectFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLUtils;
-import com.example.android.mediafx.tests.R;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
